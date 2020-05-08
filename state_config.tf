52ce074aab031eb52cfd16e6c08a2a6171c5e931
Manuela Carrasco <manuela.carrasco@endava.com>
2020-05-08 10:41:44 -0500

    Solving errors

--- state_config.tf
+++ state_config.tf
@@ -1,7 +1,7 @@
 terraform { 
     backend "s3" {
         bucket          = "terraform-eks-01"
-        key             = "terraform-eks-01/state"
+        key             = "terraform-eks-00/state"
         region          = "us-east-1"
         dynamodb_table  = "terraform-eks-01"
         encrypt         = true
